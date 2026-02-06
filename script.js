88e7dbff51282a1654681c718d060666355ebfba
Potassium3 <miles.plowman@gmail.com>
2026-02-06 21:23:38 +0000

    ERROR FIXED!

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ const rhythmLevels = {
     10: "Longer rhythms",
 }
 
+const melodyLevels = {
+    0: "Adaptive practice",
+    1: "Simple open-key melodies in treble clef",
+    2: "Melodies with a greater range of notes",
+    3: "Introducing key signatures",
+    4: "Bass clef melodies",
+    5: "More complex key signatures",
+    6: "Accidentals",
+    7: "All key signatures",
+    8: "Varied tempo",
+    9: "Other clefs",
+    10: "",
+}
+
 /*
 Details:
 {
@@ -40,18 +54,34 @@ Music:
 */
 
 function recursiveGenerate(details, duration, depth=0) {
-    let re = [0, 0]
     let split = Math.random() > 0.5 ? true : false // Random true/false
     if (depth <= 5 && split && duration >= 0.5) {
-        let duration1 = duration/2;
-        let duration2 = duration/2;
-        re[0] = recursiveGenerate(details, duration1, depth+1);
-        re[1] = recursiveGenerate(details, duration2, depth+1);
+        let re;
+        if ([3].includes(duration)) {
+            let groupTwoOne = Math.random() > 0.5 ? true : false // Random true/false
+            re = [0, 0]
+            let duration1, duration2;
+            if (groupTwoOne) {
+                duration1 = duration*2/3;
+                duration2 = duration/3;
+            } else {
+                duration1 = duration/3;
+                duration2 = duration*2/3;
+            }
+            re[0] = recursiveGenerate(details, duration1, depth+1);
+            re[1] = recursiveGenerate(details, duration2, depth+1);
+        } else {
+            re = [0, 0]
+            let duration1 = duration/2;
+            let duration2 = duration/2;
+            re[0] = recursiveGenerate(details, duration1, depth+1);
+            re[1] = recursiveGenerate(details, duration2, depth+1);
+        }
+        return re
     } else {
         // Base case
         return [duration]
     }
-    return re
 }
 
 function generateMusic(details) {
@@ -59,33 +89,51 @@ function generateMusic(details) {
     barDurations = [];
     if (details.metre[3] != 0) {
         barDurations.push(details.metre[3]/(details.metre[1]-3))
+        for (let i=0; i<details.metre[2]-1; i++) {
+            barDurations.push(details.metre[0]/(details.metre[1]-3));
+        }
+        barDurations.push((details.metre[0]-details.metre[3])/(details.metre[1]-3))
+    } else {
+        for (let i=0; i<details.metre[2]; i++) {
+            barDurations.push(details.metre[0]/(details.metre[1]-3));
+        }
     }
-    for (let i=0; i<details.metre[2]; i++) {
-        barDurations.push(details.metre[0]/(details.metre[1]-3));
-    }
+
     let barsToReturn = []
     for (let bar of barDurations) {
         barsToReturn.push(recursiveGenerate(details, duration=bar));
     }
-    console.log("BARS: "+barDurations)
     return {
         metre: details.metre,
         bars: barsToReturn,
     }
 }
 
-function collapseUpToDepth(arr, dep) {
-    if (dep == 0) {
-        return arr;
-    }
-    let newarr = [];
+function depthSum(arr) {
+    let total = 0;
     for (let item of arr) {
         if (item[0] != undefined) {
+            // item is an array
+            total += depthSum(item);
+        } else {
+            // item is a number
+            total += item
+        }
+    }
+    return total;
+}
+
+function collapseUpToDepth(arr, dep) {
+    let newarr = [];
+    for (let item of arr) {
+        if (item[0] != undefined && depthSum(item) > 1) {
+            // item is an array
             let subitems = collapseUpToDepth(item, dep-1);
             for (let subitem of subitems) {
                 newarr.push(subitem);
             }
         } else {
+            // item is a number
             newarr.push(item);
         }
     }
@@ -127,17 +175,15 @@ function crotchetExpand(arr) {
     }
 }
 
-function showMusic(music, depth) {
+function showMusic(music) {
     const elem = document.getElementById("display-music");
     let notes = "";
-    let collapsedBars = collapseUpToDepth(music.bars, depth);
+    let collapsedBars = collapseUpToDepth(music.bars, 2);
     let barDuration = music.metre[0]/(music.metre[1]-3);
-    // THE ERROR IS HERE!!!!!
     let totalDuration = 0;
     for (let note of collapsedBars) {
 
         if (note == 1) {
-            console.log("C")
             notes += `
             <div class="div-music-main-note div-music-main-note-crotchet">
                 <div class="div-music-main-note-notehead div-music-main-note-notehead-crotchet" style="top:40px;"></div>
@@ -145,22 +191,27 @@ function showMusic(music, depth) {
             </div>`
             totalDuration += 1;
         } else if (note == 2) {
-            console.log("M")
             notes += `
             <div class="div-music-main-note div-music-main-note-minim">
                 <div class="div-music-main-note-notehead div-music-main-note-notehead-minim" style="top:40px;"></div>
                 <div class="div-music-main-note-notestemdown" style="top:40px;"></div>
             </div>`
             totalDuration += 2;
+        } else if (note == 3) {
+            notes += `
+            <div class="div-music-main-note div-music-main-note-minim">
+                <div class="div-music-main-note-notehead div-music-main-note-notehead-minim" style="top:40px;"></div>
+                <div class="div-music-main-note-notestemdown" style="top:40px;"></div>
+                <div class="div-music-main-note-dot" style="top:40px;"></div>
+            </div>`
+            totalDuration += 3;
         } else if (note == 4) {
-            console.log("S")
             notes += `
             <div class="div-music-main-note div-music-main-note-semibreve">
                 <div class="div-music-main-note-notehead div-music-main-note-notehead-semibreve" style="top:40px;"></div>
             </div>`
             totalDuration += 4;
         } else {
-            console.log("SC")
             notes += `
             <div class="div-music-main-note div-music-main-note-cont">
                 ${crotchetExpand(note)}
@@ -168,7 +219,7 @@ function showMusic(music, depth) {
             totalDuration += 1;
         }
 
-        if (totalDuration%music.metre[0] == music.metre[3] && totalDuration!=0 && totalDuration!=music.metre[0]/(music.metre[1]-3)*music.metre[2]) {
+        if (totalDuration%music.metre[0] == music.metre[3] && totalDuration!=0 && totalDuration!=barDuration*music.metre[2]) {
             notes += `
             <div class="div-music-main-barline">
                 <div class="div-music-main-barline-single"></div>
@@ -196,13 +247,11 @@ function showMusic(music, depth) {
 
 function generateAndShowMusic(details) {
     let music = generateMusic(details);
-    console.log(music);
-    showMusic(music, details.depth);//{'metre':[4,4,2,0],'bars':[[[[[0.5],[0.5]],[1]],[2]],[[[1],[[0.5],[0.5]]],[[[0.5],[0.5]],[[0.5],[0.5]]]]]});
+    showMusic(music);//{'metre':[4,4,2,0],'bars':[[[[[0.5],[0.5]],[1]],[2]],[[[1],[[0.5],[0.5]]],[[[0.5],[0.5]],[[0.5],[0.5]]]]]});
 }
 
 let detailsForMusic = {
-    depth: 2, // 1 = crotchet+minim, 2 = quaver, 3 = semi
-    metre: [2, 4, 2, 0], // Time sig, bars, anacrusis duration
+    metre: [4, 4, 2, 1], // Time sig, bars, anacrusis duration
     dots: false,
     ties: false,
     rests: false,
